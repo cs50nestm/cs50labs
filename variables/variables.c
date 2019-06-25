99e7558549371ca143ff26101fb1dde7a6c113fd
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-06-25 11:04:48 -0400

    Update variables.c

--- variables/variables.c
+++ variables/variables.c
@@ -4,5 +4,14 @@
 int main(void)
 {
     // Let's assume my age is 18
-    printf("I'm %i years old!\n", 18);
+    printf("Right now I'm %i years old.\n", 18);
+    
+    // My age next year
+    printf("Next year I'll be %i years old.\n", 18 + 1);
+    
+    // My age in 10 years
+    printf("In 10 years, I'll be %i!\n", 18 + 10);
+    
+    // My age in 2050!
+    printf("In the year 2015 I'll be %i! Wow!\n", 18 + 2050 - 2019);
 }
