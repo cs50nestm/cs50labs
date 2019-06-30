39c6f46c7d7251b67acb538a0bfda788c98786d7
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-06-30 08:27:45 -0400

    Update datatypes.c

--- datatypes/datatypes.c
+++ datatypes/datatypes.c
@@ -3,6 +3,28 @@
 
 int main(void)
 {
-    int i = get_int("Integer: ");
-    printf("hello, %i\n", i);
+    // declare a string named "name" and get input with get_string
+    int name = get_string("Name: ");
+    
+    // declare an int named "age" and get user input with get_int
+    
+    
+    // declare a float named "gpa" and use get_float to enter a number with a decimal
+    
+    
+    // declare a char named "grade" and use get_char to enter a letter grade
+    
+    
+    // declare long long named "student_id" and use get_long for input
+    
+   
+    // declare a double named "pi" and use get_double to accept user input to 10 decimal placees
+    
+    
+    printf("Hello %s, here is the data you entered...\n", name);
+    printf("Student ID: %lld\n" studend_id);
+    printf("Age: %i\n", age);
+    printf("GPA: %f\n", gpa);
+    printf("Grade: %c\n", grade);
+    printf("The value of pi is %f\n", pi);
 }
