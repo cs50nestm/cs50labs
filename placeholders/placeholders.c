de29733595a0e840db8c896380f8eba5fd0b5990
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-06-30 12:10:23 -0400

    Update placeholders.c

--- placeholders/placeholders.c
+++ placeholders/placeholders.c
@@ -3,6 +3,15 @@
 
 int main(void)
 {
-
+  // declare a string animal and get user input
+  string animal = get_string("Enter a a type of animal: ");
+  
+  // declare a celestial body and get input
+  string body = get_string("Enter a celesial body: ");
+  
+  // declare a number and get input
+  int number = get_int("Enter a whole number: ");
+  
+  // write your printf statement here
 
 }
