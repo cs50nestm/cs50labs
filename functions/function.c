1de9603051415a6f3a29fd4adaefe42b0ab6ab2d
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-07-12 11:36:25 -0400

    Update function.c

--- functions/function.c
+++ functions/function.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_positive_int(string prompt)
-{
-    return INT_MAX;
-}
+int get_positive_int(string prompt);
 
 int main(void)
 {
     int n = get_positive_int("Enter a positive number: ");
+}
+
+int get_positive_int(string prompt)
+{
+    return INT_MAX;
 }
