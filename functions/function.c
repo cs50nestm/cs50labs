e92d4b3debea297f3dceb0ff44b9a054c77e8de5
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-07-12 11:39:30 -0400

    Update function.c

--- functions/function.c
+++ functions/function.c
@@ -6,6 +6,9 @@ int get_positive_int(string prompt);
 int main(void)
 {
     int n = get_positive_int("Enter a positive number: ");
+    
+    // print the output from our function
+    printf("My positive int is %i.\n", n);
 }
 
 int get_positive_int(string prompt)
