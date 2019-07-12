5338cd9626dec0be140b96f408f071945a65f59e
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-07-12 11:35:55 -0400

    Update and rename syntax.c to function.c

---
+++ functions/function.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(string prompt)
+{
+    return INT_MAX;
+}
+
+int main(void)
+{
+    int n = get_positive_int("Enter a positive number: ");
+}
--- functions/syntax.c
+++
@@ -1,5 +0,0 @@
-include stdio.h  
-{
-    int main(void)
-    printf("This is CS50AP!\n")
-}
