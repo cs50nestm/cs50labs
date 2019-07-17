8c1669d6396112e0518b01f82b7779edafa5b948
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-07-17 11:14:49 -0400

    Update exit.c

--- exitcodes/exit.c
+++ exitcodes/exit.c
@@ -3,13 +3,13 @@
 
 int main(int argc, string argv[])
 {
-  if (argc != 3)
-  {
-    printf("Invalid number of command-line arguments!\n");
-  }
-  else
-  {   
-    // prints out hello followed by firstname, lastname  
-    printf("hello, %s %s\n", argv[1], argv[2]);
-  }
+    if (argc != 3)
+    {
+        printf("Invalid number of command-line arguments!\n");
+    }
+    else
+    {
+      // prints out hello followed by firstname, lastname  
+      printf("hello, %s %s\n", argv[1], argv[2]);
+    }
 }
