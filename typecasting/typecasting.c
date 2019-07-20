2f019cce9287cd2fecd1ae799906bf7dc89b40c4
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-07-20 10:55:29 -0400

    Update typecasting.c

--- typecasting/typecasting.c
+++ typecasting/typecasting.c
@@ -1,28 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    // declare a string named "name" and get input with get_string
-    string name = get_string("Name: ");
+    // get a string named plaintext
+    string plaintext = get_string("plaintext ");
     
-    // declare an int named "age" and get user input with get_int
+    // output the string
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        printf("%c", plaintext[i]);
+    }
     
-    
-    // declare a float named "gpa" and use get_float to enter a number with a decimal
-        
-    
-    // declare long long named "student_id" and use get_long for input
-    
-   
-    // declare a double named "pi" and use get_double to accept user input to 10 decimal placees
-    
-    
-    // prints a blank line followed by each input value
     printf("\n");
-    printf("Hello %s, here is the data you entered...\n", name);
-    printf("Student ID: %lld\n", student_id);
-    printf("Age: %i\n", age);
-    printf("GPA: %.1f\n", gpa);
-    printf("The value of pi is %.10f\n", pi);
 }
