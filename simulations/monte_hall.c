0b6fcd3375c55e92bc2b3ecfa9fbb738cae681d1
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-01 17:07:17 -0400

    Create monte_hall.c

---
+++ simulations/monte_hall.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+ 
+#define GAMES 3000000
+ 
+int main(void)
+{
+    srand(time(NULL));  
+    
+    int win_by_switch = 0;
+    
+    int door[3], choice;
+    
+    for (int i = 0; i < GAMES; i++)
+    {
+        // door1 gets 0 or 1, goat or car
+        door[0] = rand() % 2; 
+        
+        // if door[0] has car, give other doors goats
+        if (door[0] == 1)
+        {
+            door[1] = 0;
+            door[2] = 0;
+        }
+        // else, give door2 car or goat, give door3 what's left.
+        else
+        {
+            door[1] = rand() % 2;
+            door[2] = (door[1] == 1) ? 0 : 1;
+        }
+        
+        // choose a random door
+        choice = rand() % 3;
+        
+        // since the host will only open a door with a goat, if either of the other 
+        // doors has the car, the player wins if they switch
+        if (door[(choice + 1) % 3] == 1 || door[(choice + 2) % 3] == 1)
+        {
+            win_by_switch++;
+        }
+    }
+                                                
+    printf("Out of %i games, you would win %i times by switching!\n", GAMES, win_by_switch);                                            
+}
