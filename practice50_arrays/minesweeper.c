5b03bdcedbd57f8ceb4d03dee2e5047b1095292a
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-01 21:27:20 -0400

    Create minesweeper.c

---
+++ practice50_arrays/minesweeper.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <time.h>
+
+#define HEIGHT 10
+#define WIDTH 8
+#define MINE_NUM 5
+
+int main(void)
+{
+    // initialize field to be empty, and fill with 5 mines randomly located
+    int field[HEIGHT][WIDTH] = {{0}};
+    time_t t;
+    srand((unsigned) time(&t));
+    for (int k = 0; k < MINE_NUM; k++)
+    {
+        int i = rand() % HEIGHT;
+        int j = rand() % WIDTH;
+        // if mine has been planted in this location already, try again
+        if (field[i][j] == 1)
+            k--;
+        else
+            field[i][j] = 1;
+    }
+    
+    // TODO: print the location of the mines!
+
+
+}
