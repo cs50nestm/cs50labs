da6370897ca2450739def3b308d0e1595d1f1bd5
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-01 21:31:41 -0400

    Update minesweeper.c

--- practice50_arrays/minesweeper.c
+++ practice50_arrays/minesweeper.c
@@ -11,8 +11,10 @@ int main(void)
     // initialize field to be empty, and fill with 5 mines randomly located
     int field[HEIGHT][WIDTH] = {{0}};
     
+    //initialize random seed
     srand(time(NULL));       
     
+    // iterate through 2-dimensional array to place mines
     for (int k = 0; k < MINE_NUM; k++)
     {
         int i = rand() % HEIGHT;
