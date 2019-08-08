716980caff5ec537a064337bf8d16531e25fb28b
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-08 13:14:22 -0400

    Create mystruct.c

---
+++ structs/mystruct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    typedef struct
+    {
+        // members get defined here
+
+        
+    }
+    // the name of your new data type follows
+    name_goes_here;
+    
+    // declare a new variable using your new data type
+ 
+    
+    // add data to your new structure
+    
+    
+    // print out the data in your structure
+    
+}
