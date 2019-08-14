eb4898d8eaa009ab05a3cf3917dd2f165094cb1a
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-14 10:56:25 -0400

    Update fileio.c

--- fileio/fileio.c
+++ fileio/fileio.c
@@ -6,19 +6,24 @@ int main(int argc, string argv[])
     // verify there is a correct number of arguments in the command line
     if (argc != 3)
     {
-        printf("Usage: cp source dest\n");
+        printf("Usage: fileio source dest\n");
         return 1;
     }
 
-    // open up the source file (if the file doesn't exist, print an error message and return)
+    // open up the source file, argv[1] for reading
+    
+    
+    // if the file doesn't exist, (the file pointer is NULL) print an error message and return
     
 
-    // now open up the destination file (remember to check if NULL, and if so be sure to close the source file before exiting!)
+    // now open up the destination file, argv[2]
+    
+    
+    // check if NULL, and if so be sure to close the source file before exiting
     
 
-    // read the contents of the source file one char (int!) at a time, and write to the destination file
-    // remember to use int so that the c != EOF comparison is reliable
-
+    // read the contents of the source file one char at a time, and write to the destination file
+    
     
     // close the source and destination files
     
