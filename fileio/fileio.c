60af2c6545bebe8e5f80db6d225ff2b251fa8ded
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-14 10:39:31 -0400

    Create fileio.c

---
+++ fileio/fileio.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    // verify there is a correct number of arguments in the command line
+    if (argc != 3)
+    {
+        printf("Usage: cp source dest\n");
+        return 1;
+    }
+
+    // open up the source file (if the file doesn't exist, print an error message and return)
+    
+
+    // now open up the destination file (remember to check if NULL, and if so be sure to close the source file before exiting!)
+    
+
+    // read the contents of the source file one char (int!) at a time, and write to the destination file
+    // remember to use int so that the c != EOF comparison is reliable
+
+    
+    // close the source and destination files
+    
+}
