c0417fbde34ec5ff2f008f5b1cfb11a9a98eb303
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-08-15 21:57:18 -0400

    Update helpers.c

--- images/helpers.c
+++ images/helpers.c
@@ -2,16 +2,5 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (image[i][j].rgbtRed == 0xff)
-            {
-                image[i][j].rgbtRed = 0x00;
-                image[i][j].rgbtGreen = 0xff;
-            }
-
-        }
-    }
+    // TODO
 }
