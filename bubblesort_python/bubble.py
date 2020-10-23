cce2c457064c4f4815c531141a27b80cb8aa57ef
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-10-23 14:19:44 -0400

    Update and rename bubble.c to bubble.py

---
+++ bubblesort_python/bubble.py
@@ -0,0 +1,21 @@
+# Complete the bubble sort function
+
+# List of numbers to sort
+numbers = [1, 8, 4, 6, 0, 3, 5, 2, 7, 9]
+
+
+def main():
+    bubble_sort(numbers)
+
+    for number in numbers:
+        print(number, end=" ")
+    print()
+
+
+# Sort list using bubble sort
+def bubble_sort(numbers):
+    # TODO
+    return
+
+
+main()
