39ec484303754c2f28c847574bf990b005e0efc6
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-10-30 09:17:44 -0400

    Update and rename selection.c to selection.py

---
+++ selectionsort_python/selection.py
@@ -0,0 +1,21 @@
+# Complete the selection sort function
+
+# List of numbers to sort
+numbers = [1, 8, 4, 6, 0, 3, 5, 2, 7, 9]
+
+
+def main():
+    selection_sort(numbers)
+
+    for number in numbers:
+        print(number, end=" ")
+    print()
+
+
+# Sort list using selection sort
+def selection_sort(numbers):
+    # TODO
+    return
+
+
+main()
