bcff11ab9eb8ea3ca1c2c7a1201274b1b801cfd9
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-11-22 17:29:24 -0500

    Update and rename binary.c to binary.py

---
+++ binarysearch_python/binary.py
@@ -0,0 +1,21 @@
+# implement binary search function
+
+from cs50 import get_int
+
+numbers = [2, 4, 6, 7, 8, 11, 14, 18, 20]
+
+def main:
+    target = get_int("Enter a number: ")
+
+    if bin_search(numbers, target):
+        print("Found")
+    else:
+        print("Not found!")
+
+            
+def bin_search(numbers, target):
+    // TODO
+    return False;
+
+
+main()
